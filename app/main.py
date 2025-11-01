e8f94d8d7e170100abf111aa6c699b660411a878
Master <hotrongbao27@gmail.com>
2025-11-01 17:42:25 +0700

    Xóa thiết lập FastAPI và giữ lại câu lệnh in Hello World cơ bản

--- app/main.py
+++ app/main.py
@@ -1,13 +1,1 @@
-from fastapi import FastAPI
-from app.api.v1.router import api_router
-from app.config.database import Base, engine
-
-app = FastAPI(title="Verification Service")
-
-app.include_router(api_router, prefix="/api/v1")
-
-
-@app.on_event("startup")
-def on_startup():
-    # create tables (for demo/simple setups). In production use migrations (Alembic)
-    Base.metadata.create_all(bind=engine)
+print("Hello, World!")
